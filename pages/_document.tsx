4812ec92ab95d3c06086ae89f5f27b711dde6a04
Erick-Bravo <erickbrvo@gmail.com>
2024-06-18 12:11:57 -0700

    smaller screen sizes fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,16 +2,27 @@ import { ChakraProvider, ThemeProvider, extendTheme } from "@chakra-ui/react";
 // import { ThemeProvider } from "@chakra-ui/core"
 import { Html, Head, Main, NextScript } from "next/document";
 
+// 2. Update the breakpoints as key-value pairs
+const breakpoints = {
+  base: "0px",
+  sm: "390px",
+  md: "768px",
+  lg: "960px",
+  xl: "1200px",
+  "2xl": "1536px",
+};
+
+// 3. Extend the theme
+const theme = extendTheme({ breakpoints });
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <ChakraProvider>
-          
+        <ChakraProvider theme={theme}>
             <Main />
             <NextScript />
-        
         </ChakraProvider>
       </body>
     </Html>
--- pages/mobile.tsx
+++ pages/mobile.tsx
@@ -1,59 +1,138 @@
 import { BannerText } from "@/components/Home/Banner";
-import { Box, Text, Image, Button, Flex } from "@chakra-ui/react";
-import { PhoneIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Text,
+  Image,
+  Button,
+  Flex,
+  UnorderedList,
+  ListItem,
+  List,
+  ListIcon,
+} from "@chakra-ui/react";
+import { PhoneIcon, CheckCircleIcon } from "@chakra-ui/icons";
 
 const CallButton = () => {
   return (
     <Flex justifyContent="center" alignItems="center" w="100%">
-      <Button
+      <Flex
         bg="#213952"
+        // width={{ base: "200px", sm: "400px" }}
         w="200px"
-        h="60px"
-        fontSize="30px"
+        // h={{ base: "40px", sm: "60px" }}
+        h="44px"
+        fontSize="25px"
         borderRadius="15px"
-        // textAlign="center"
+        justifyContent="center"
+        alignItems="center"
       >
+        <PhoneIcon h="15px" />
         <a href="tel:9097676356"> Call Now</a>
-      </Button>
+      </Flex>
     </Flex>
+  );
+};
+
+const BannerMobile = () => {
+  return (
+    <Box
+      position="relative"
+      width="100%"
+      h="350px"
+      backgroundImage="url('/cucamobile.jpg')"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+    >
+      <Flex
+        position="absolute"
+        top="0"
+        left="0"
+        width="100%"
+        h="100%"
+        bg="white"
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor="rgba(0, 0, 0, 0.5)" // Adjust the opacity as needed
+      >
+        <Flex
+          w="100vw"
+          justifyContent="center"
+          alignItems="center"
+          h="100%"
+          flexDir="column"
+          p="40px 20px"
+        >
+          <Image src="/yoslogo.svg" alt="logo" w={["50%"]} />
+          <BannerText text="Bringing The Fresh Mountain Air" />
+          <BannerText text="To The Inland Empire" />
+          <BannerText text="One Home At A Time" />
+        </Flex>
+      </Flex>
+    </Box>
   );
 };
 
 const mobile = () => {
   return (
     <Flex
-      justifyContent="center"
-      alignItems="center"
-      flexDir="column"
       bg="#00789E"
-      h="100vh"
-      border="2px solid black"
-      pt="80px"
+      h={{ base: "900px", sm: "100vh" }}
+      flexDir="column"
+      justifyContent="space-between"
+      alignItems="center"
     >
+      <BannerMobile />
+
+      <Box mt="40px">
+        <List fontWeight="bold">
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} mr="15px" />
+            Repairs
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} mr="15px" />
+            Maintenence
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} mr="15px" />
+            Tune-up $50
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} mr="15px" />
+            Ductwork Inspection
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} mr="15px" />
+            Efficiency Evaluation
+          </ListItem>
+        </List>
+      </Box>
+
       <Flex
-        w="100vw"
-        h="100%"
+        w="260px"
+        flexDir="column"
         justifyContent="center"
         alignItems="center"
-        flexDir="column"
+        mt="40px"
       >
-        <Image src="/yoslogo.svg" alt="logo" h="250px" />
-        <BannerText text="Bringing The Fresh Mountain Air" />
-        <BannerText text="To The Inland Empire" />
-        <BannerText text="One Home At A Time" />
+        <BannerText text="Cheaper and more reliable than the corporate guys" />
+        <BannerText text="gauranteed" />
       </Flex>
 
-      <Flex mt="30px" justifyContent="space-around" alignItems="center">
-        <CallButton />
+      <Flex alignItems="center" flexDir="column">
+        <Flex mt="50px" alignItems="center">
+          <CallButton />
+        </Flex>
+
+        <Text fontSize="20px" m="10px 0">
+          Call For A Free Diagnosis
+        </Text>
+
+        <Box bg="white" w="100%" mt="40px">
+          <Image src="/ProfessionalUpdated.svg" alt="ad" />
+        </Box>
       </Flex>
-
-      <Text fontSize="20px" m="10px 0">
-        Call For A Free Diagnosis
-      </Text>
-
-      <Box bg="white" w="100%">
-        <Image src="/ProfessionalUpdated.svg" alt="ad" />
-      </Box>
     </Flex>
   );
 };
